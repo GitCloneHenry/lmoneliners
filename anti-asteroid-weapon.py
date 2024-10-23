68ff0b8bc86536a2711220334ec1ab37202242f4
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-23 16:02:38 -0600

    fixed again

--- anti-asteroid-weapon.py
+++ anti-asteroid-weapon.py
@@ -1,4 +1,4 @@
 print('\n'.join(
         sorted(
-            point for point in (list(map(float, input().split(' '))) for j in [0]*int(input()))), 
-            key=lambda point : (point[0] ** 2 + point[1] ** 2) ** 0.5) for i in [0]*int(input()))
+            (point for point in (list(map(float, input().split(' '))) for j in [0]*int(input()))), 
+            key=lambda point : (point[0] ** 2 + point[1] ** 2) ** 0.5) for i in [0]*int(input())))
