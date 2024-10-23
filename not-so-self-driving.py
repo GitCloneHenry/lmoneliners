dd8a2da096a292f17f86b3a803867b37ddc6ca3d
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-22 19:26:21 -0600

    Fixed

--- not-so-self-driving.py
+++ not-so-self-driving.py
@@ -1,3 +1,3 @@
 for i in [0]*int(input()):
-    a, b = input().split(':')
+    a, b = [int(element) for element in input().split(':')]
     print(['SWERVE', 'BRAKE', 'SAFE'][b / a <= 1 + b / a <= 5 - 1])
