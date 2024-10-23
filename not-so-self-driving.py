c03c3a3d715baf94478a6a8e361fc7428b8cac2e
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-22 19:33:08 -0600

    Another try

--- not-so-self-driving.py
+++ not-so-self-driving.py
@@ -1,3 +1,3 @@
 for i in [0]*int(input()):
     a, b = [float(element) for element in input().split(':')]
-    print(['SWERVE', 'BRAKE', 'SAFE'][b / min(a,0.001) <= 1 + b / min(a,0.001) <= 5 - 1])
+    print(['SWERVE', 'BRAKE', 'SAFE'][(b <= a) + (b <= 5 * a) - 1])
