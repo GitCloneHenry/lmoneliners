db6a026577ddaa8f6f851299781bd55ca1a10e1f
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-23 11:52:29 -0600

    What the hell is this program bro

--- anti-asteroid-weapon.py
+++ anti-asteroid-weapon.py
@@ -1,1 +1,1 @@
-print('\n'.join('\n'.join() for i in [0]*int(input())))
+print('\n'.join('\n'.join(sorted(list(str(a, b) for a, b in (list(map(float, input().split(' '))) for j in [0]*int(input()))), key=lambda point : (point[0] ** 2 + point[1] ** 2) ** 0.5) for i in [0]*int(input()))))
