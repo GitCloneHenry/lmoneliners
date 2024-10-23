c61bae0529580cd247b8aee59ba3b6ad093e6ce7
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-23 16:19:40 -0600

    testing more

--- anti-asteroid-weapon.py
+++ anti-asteroid-weapon.py
@@ -1,3 +1,3 @@
 for i in [0]*int(input()):
     # print('\n'.join([f"{a} {b}" for a, b in sorted([map(int, input().split(' ')) for j in [0]*int(input())], key=lambda a, b: (a**2 + b**2)**0.5)]))
-    print(sorted([map(int, input().split(' ')) for j in [0]*int(input())], key=lambda a, b: (a**2 + b**2)**0.5))
+    print(sorted([list(map(int, input().split(' '))) for j in [0]*int(input())], key=lambda a, b: (a**2 + b**2)**0.5))
