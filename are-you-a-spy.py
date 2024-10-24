7b68063b8bc8b98a1cd140863ce34fed243aadc3
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-23 19:25:35 -0600

    even more testing

--- are-you-a-spy.py
+++ are-you-a-spy.py
@@ -1,3 +1,6 @@
 for i in [0]*int(input()):
-    print(((sum(a.count(letter) for letter in b) < len(b)) for a, b in '\n'.join(map(str, [letter for letter in input().lower() if letter in 'abcdefghijklmnopqrstuvwxyz'])).split('|')))
+    print(''.join(map(str, [letter for letter in input().lower() if letter in 'abcdefghijklmnopqrstuvwxyz'])).split('|'))
+    # print(((
+    #     sum(a.count(letter) for letter in b) < len(b)) for a, b in ''.join(
+    #         map(str, [letter for letter in input().lower() if letter in 'abcdefghijklmnopqrstuvwxyz'])).split('|')))
     # print('You\'re not a secret agent!' if ((sum(a.count(letter) for letter in b) < len(b)) for a, b in '\n'.join(map(str, [letter for letter in input().lower() if letter in 'abcdefghijklmnopqrstuvwxyz'])).split('|')) else 'That\'s my secret contact!')
