dbf81abac7857165ea46e20a5daa85386081d336
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-23 19:58:38 -0600

    IT'S DONE

--- are-you-a-spy.py
+++ are-you-a-spy.py
@@ -1,3 +1,1 @@
 print('\n'.join("You're not a secret agent!" if ((lambda str : sum(str[0].count(char) > 0 for char in str[1]) < len(str[1]))(''.join(c for c in input().lower() if c.isalpha() or c == '|').split('|'))) else 'That\'s my secret contact!' for i in [0]*int(input())))
-
-    
