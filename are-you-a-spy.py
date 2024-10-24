ed8290843e9838a4b00cc34ee2f1cabefe651327
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-23 19:26:07 -0600

    I might have found it

--- are-you-a-spy.py
+++ are-you-a-spy.py
@@ -1,5 +1,5 @@
 for i in [0]*int(input()):
-    print(''.join(map(str, [letter for letter in input().lower() if letter in 'abcdefghijklmnopqrstuvwxyz'])).split('|'))
+    print(''.join(map(str, [letter for letter in input().lower() if letter in 'abcdefghijklmnopqrstuvwxyz|'])).split('|'))
     # print(((
     #     sum(a.count(letter) for letter in b) < len(b)) for a, b in ''.join(
     #         map(str, [letter for letter in input().lower() if letter in 'abcdefghijklmnopqrstuvwxyz'])).split('|')))
