39e312fee04030dd66aaa6c2a15f212ce2a4e389
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-25 09:45:20 -0600

    made one line

--- autocorrect.py
+++ autocorrect.py
@@ -1,13 +1,1 @@
-compare_strings = lambda a, b: sum(1 for a, b in zip(a, b) if a == b)
-
-for i in [0]*int(input()):
-    a, b = list(map(int, input().split(' ')))
-
-    words = [input() for j in [0]*a]
-
-    compared = []
-
-    for j in [0]*b:
-        wrong_word = input()
-        compared.append([compare_strings(wrong_word, word) for word in words])
-    print('\n'.join(words[compared[j].index(min(compared[j]))] for j in [0]*b))
+[(lambda lengths, words : (lambda compared : print('\n'.join(words[compared[j].index(max(compared[j]))] for j in lengths[1])))([(lambda wrong_word: [(lambda a, b: sum(1 for a, b in zip(a, b) if a == b) - abs(len(a) - len(b)))(wrong_word, word) for word in words])(input()) for j in lengths[1]]))(lengths := list(map(lambda length: range(int(length)), input().split(' '))), (words := [input() for j in lengths[0]])) for i in [0]*int(input())]
