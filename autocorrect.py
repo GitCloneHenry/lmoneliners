f5ad2a9cf86921730f982616ecb50ff7b7c3c2e4
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-25 08:34:41 -0600

    Small change to comparison

--- autocorrect.py
+++ autocorrect.py
@@ -1,4 +1,4 @@
-compare_strings = lambda a, b: sum(1 for a, b in zip(a, b) if a != b) + abs(len(a) - len(b))
+compare_strings = lambda a, b: sum(1 for a, b in zip(a, b) if a == b)
 
 for i in [0]*int(input()):
     a, b = list(map(int, input().split(' ')))
