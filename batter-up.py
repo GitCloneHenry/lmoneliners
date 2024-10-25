25e820ef2c8f2ac7ee75e6cb317fc39f56022fe9
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-25 10:22:00 -0600

    small fix

--- batter-up.py
+++ batter-up.py
@@ -1,5 +1,5 @@
 for i in [0]*int(input()):
-    name, hits = list(map(lambda str : str.split('\,'), input().split(':')))
+    name, hits = list(map(lambda str : str.split(','), input().split(':')))
 
     percentage = sum(['K', '1B', '2B', '3B', 'HR'].index(hit) for hit in hits)
     print(f"{name}={0 if not percentage else percentage / len(hits)}")
