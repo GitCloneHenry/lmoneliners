3fced35b39e36e369c0f5d63480b88bf50384e6d
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-25 14:16:00 -0600

    fixed

--- brick-house.py
+++ brick-house.py
@@ -1,1 +1,1 @@
-print('\n'.join(['false', 'true'][c - 5 * b > a] for a, b, c in (input().split() for i in [0]*int(input()))))
+print('\n'.join(['false', 'true'][c - 5 * b > a] for a, b, c in (list(map(int, input().split())) for i in [0]*int(input()))))
