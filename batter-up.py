b83b4599d24110e403aff34d2215571a0133f7a0
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-25 11:35:29 -0600

    Update batter-up.py

--- batter-up.py
+++ batter-up.py
@@ -1,5 +1,5 @@
 for i in [0]*int(input()):
     name, hits = list(map(lambda str : str.split(','), input().split(':')))
 
-    percentage = sum(list(['K', '1B', '2B', '3B', 'HR']).index(hit) for hit in hits if hit != 'BB')
-    print(f"{name}={0 if not percentage else percentage / len(hits)}")
+    percentage = [list(['K', '1B', '2B', '3B', 'HR']).index(hit) for hit in hits if hit != 'BB']
+    print(f"{name[0]}={'0.000' if not len(percentage) else (lambda number: int(number * 1000 + 1) * 0.001 if (number * 1000 - int(number * 1000) >= 0.5) else round(number, 3))(sum(percentage) / len(percentage)):.3f}")
