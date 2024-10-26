33dd5aee4ddb1c4b651517c7928e07d2a05e63ca
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-25 18:56:53 -0600

    I found it

--- by-the-book.py
+++ by-the-book.py
@@ -1,2 +1,2 @@
 # [(lambda isbn : print('\n'.join(['INVALID', 'VALID'][{10: 'x'}.get(diff := abs((total := sum(a * b for a, b in [(index, int({'X': 10}.get(char, char))) for index, char in enumerate(isbn)])) - (-(-total // 11) * 11)), str(diff)) == isbn[-1]])))(input()) for i in [0]*int(input())]
-[(lambda isbn : print(diff := abs((total := sum(a * b for a, b in [(index, int({'X': 10}.get(char, char))) for index, char in enumerate(isbn)])))))(input()) for i in [0]*int(input())]
+[(lambda isbn : print(diff := abs((total := sum((10 - a) * b for a, b in [(index, int({'X': 10}.get(char, char))) for index, char in enumerate(isbn)])))))(input()) for i in [0]*int(input())]
