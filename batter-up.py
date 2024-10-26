9a52f629f87c835c26125c0b2035e0c77acb44f4
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-25 19:47:48 -0600

    Get fricked nerd

--- batter-up.py
+++ batter-up.py
@@ -1,1 +1,1 @@
-from statistics import mean; [(lambda a: (lambda in_data: print(f"{a[0]}={0 if not in_data else mean(in_data):.3f}"))([['K', '1B', '2B', '3B', 'HR'].index(hit) for hit in a[1].split(",") if not hit in ['BB', '']]))([i for i in input().split(':')]) for i in [0]*int(input())]
+[(lambda batter: (lambda percentage: print(f"{batter[0]}={0 if not percentage else (int(sum(percentage) / len(percentage) * 1000 + 0.5) / 1000):.3f}"))([['K', '1B', '2B', '3B', 'HR'].index(hit) for hit in batter[1].split('\n') if not hit in ['BB', '']]))(input().split(':')) for i in [0]*int(input())]
