65df88562163f8be63017d42fb6bd1b0659beb04
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-25 18:30:18 -0600

    test

--- by-the-book.py
+++ by-the-book.py
@@ -1,2 +1,2 @@
-test = lambda isbn : sum(a * int(b) if b != 'X' else a * 10 for a, b in enumerate(input().split()))
+test = lambda: sum(a * int(b) if b != 'X' else a * 10 for a, b in enumerate(input().split()))
 print('\n'.join(test() for i in [0]*int(input())))
