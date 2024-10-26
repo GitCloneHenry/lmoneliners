d986883a2b9e54e0696bd9d8926846468768c67b
Henry W <164814454+GitCloneHenry@users.noreply.github.com>
2024-10-25 18:41:50 -0600

    Even more testing

--- by-the-book.py
+++ by-the-book.py
@@ -1,2 +1,2 @@
 test = lambda: sum(a * b for a, b in [(index, int({'X': 10}.get(char, char))) for index, char in enumerate(input())])
-print('\n'.join(test() for i in [0]*int(input())))
+print('\n'.join(str(test()) for i in [0]*int(input())))
